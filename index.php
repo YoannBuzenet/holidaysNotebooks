40c002433e7fa5b247250ca9be900692329275d3
Yoann <ybuzenet@gmail.com>
2019-08-13 21:03:55 +0200

    setting up github on HN

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="homepage.css">
+	<title>Template</title>
+</head>
+<body>
+	<div class="main-container">
+		<div class="left-content">
+
+		</div>	
+		<div class="main-content">
+			<header class="main-header">
+				<div class="container">
+					<h1><a href="/">Cahiers de vacances gratuits</a></h1>
+				</div>
+				<div class="menu">
+					<ul>
+						<li><a href="#">Primaire</a></li>
+						<li><a href="#">Collège</a></li>
+					</ul>
+				</div>	
+			</header>
+			<div class="container">
+				<main>
+					<div class="cards-container">
+						<a href="#" class="card-link">
+							<article class="card">
+								<h1>3ème - College</h1>
+								<p class="discover">Découvrez</p>
+							</article>
+						</a>
+						<a href="#" class="card-link">
+							<article class="card">
+								<h1>4ème - College</h1>
+								<p class="discover">Découvrez</p>
+							</article>
+						</a>
+						<a href="#" class="card-link">
+							<article class="card">
+								<h1>5ème - College</h1>
+								<p class="discover">Découvrez</p>
+							</article>
+						</a>
+						<a href="#" class="card-link">
+							<article class="card">
+								<h1>6ème - College</h1>
+								<p class="discover">Découvrez</p>
+							</article>
+						</a>
+						<a href="#" class="card-link">
+							<article class="card">
+								<h1>CM2 - Primaire</h1>
+								<p class="discover">Découvrez</p>
+							</article>
+						</a>
+						<a href="#" class="card-link">
+							<article class="card">
+								<h1>CM1 - Primaire</h1>
+								<p class="discover">Découvrez</p>
+							</article>
+						</a>
+						<a href="#" class="card-link">
+							<article class="card">
+								<h1>CE2 - Primaire</h1>
+								<p class="discover">Découvrez</p>
+							</article>
+						</a>
+						<a href="#" class="card-link">
+							<article class="card">
+								<h1>CE1 - Primaire</h1>
+								<p class="discover">Découvrez</p>
+							</article>
+						</a>
+						<a href="#" class="card-link">
+							<article class="card">
+								<h1>CP - Primaire</h1>
+								<p class="discover">Découvrez</p>
+							</article>
+						</a>
+					</div>	
+				</main>
+			</div>
+		</div>
+		<div class="right-content">
+			
+		</div>
+	</div>
+</body>
+</html>
